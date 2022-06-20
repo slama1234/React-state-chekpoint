7b2459f9939d2e7e8b4c495208b4612d36b4760b
Mustapha Slama <mustaphaslama141@gmail.com>
2022-06-20 10:29:43 +0200

    react state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component { 
+  constructor(){
+    super()
+    this.state = {
+      count: 0 
+    }
+
+  }
+
+  increment = ()=>{
+    this.setState({count:this.state.count +1})
+  }
+
+  reset = ()=>{
+    this.setState({count:0})
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.increment}>+</button>
+        <h3> {this.state.count} </h3>
+        <button onClick={()=>this.setState({count:this.state.count -1})}>-</button>
+        <button onClick={this.reset}>Reset</button>
+      </div>
+    )
+  }
 }
-
-export default App;
